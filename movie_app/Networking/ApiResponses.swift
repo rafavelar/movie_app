8f15045127f1673e130e8d91910e508ba214834d
ravelar <rafaelksd@live.com>
2020-11-18 18:05:07 -0600

    Create the ApiResponse data

---
+++ movie_app/Networking/ApiResponses.swift
@@ -0,0 +1,24 @@
+//
+//  ApiResponses.swift
+//  movie_app
+//
+//  Created by Rafael Avelar on 18/11/20.
+//
+
+import Foundation
+
+struct TvShow: Codable {
+  let id: Int
+  let url: String
+  let name: String
+  let type: String
+  let language: String
+  let genres: [String]
+  let status: String
+  let image: Image
+}
+
+struct Image: Codable {
+  let medium: String
+  let original: String
+}
---
+++ movie_app/Networking/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  movie_app
+//
+//  Created by Rafael Avelar on 18/11/20.
+//
+
+import Foundation
