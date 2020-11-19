e81d08323b1f5a468dcd9ec8569265d7602badba
ravelar <rafaelksd@live.com>
2020-11-18 18:22:23 -0600

    Create the fetchShows function into NetworkManager

--- movie_app/Networking/NetworkManager.swift
+++ movie_app/Networking/NetworkManager.swift
@@ -9,4 +9,28 @@ import Foundation
 
 public final class NetworkManager {
   
+  var shows: [Show] = []
+  private let domainUrlString = "http://api.tvmaze.com/"
+  
+  func fetchShows(completionHandler: @escaping ([Show]) -> Void){
+    let url = URL(string: domainUrlString + "shows")!
+    
+    let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+      if let error = error {
+        print("Error with fetching shows: \(error)")
+      }
+      
+      guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+        print("Error with the response, unexpected status code: \(response ?? "No error")")
+        return
+      }
+      
+      if let data = data,
+         let showSummary = try? JSONDecoder().decode(ShowSummary.self, from: data) {
+        completionHandler(showSummary.results ?? [])
+      }
+    })
+    task.resume()
+  }
+  
 }
