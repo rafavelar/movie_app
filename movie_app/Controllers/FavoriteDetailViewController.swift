42b8b560f5373494bc72a1f132fd924b34e0b64d
ravelar <rafaelksd@live.com>
2020-11-20 14:52:59 -0600

    #5 Set outlest and open imdb page

--- movie_app/Controllers/FavoriteDetailViewController.swift
+++ movie_app/Controllers/FavoriteDetailViewController.swift
@@ -12,32 +12,47 @@ class FavoriteDetailViewController: UIViewController {
   var show: ShowEntity?
   
   @IBOutlet weak var imageShowDetail: UIImageView!
-   @IBOutlet weak var summaryShowDetail: UITextView!
-   
-   @IBOutlet weak var nameShowDetail: UILabel!
-   @IBOutlet weak var languageShowDetail: UILabel!
-   @IBOutlet weak var statusShowDetail: UILabel!
-   @IBOutlet weak var imdbShowDetail: UILabel!
+  @IBOutlet weak var summaryShowDetail: UITextView!
+  
+  @IBOutlet weak var nameShowDetail: UILabel!
+  @IBOutlet weak var languageShowDetail: UILabel!
+  @IBOutlet weak var statusShowDetail: UILabel!
+  @IBOutlet weak var imdbShowDetail: UILabel!
   @IBOutlet weak var IMDbButton: UIButton!
+  
+  var navButton: UIBarButtonItem? = nil
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     // Do any additional setup after loading the view.
     if let showDetail = show {
+      
+      self.tabBarController?.navigationItem.title = showDetail.name
+      
       setShowInfo(show: showDetail);
       
-      if((showDetail.imdb?.isEmpty) != nil){
-        //IMDbButton.isHidden = true
+      if let imdb = showDetail.imdb {
+        if(imdb.isEmpty){
+          IMDbButton.isHidden = true
+        }
       }
     }
     
+    
+    navButton = UIBarButtonItem(title: "Delete", style: .plain, target: self, action: #selector(deleteFavorite))
+    navigationItem.rightBarButtonItem = navButton
+    
   }
   @IBAction func openIMDb(_ sender: Any) {
-    
-    if let url = URL(string: "https://www.imdb.com/title/\(String(describing: show?.imdb))/") {
+    if let imdb = show?.imdb{
+      if let url = URL(string: "https://www.imdb.com/title/\(imdb)/") {
         UIApplication.shared.open(url)
+      }
     }
+  }
+  
+  @objc func deleteFavorite(_ sender: Any) {
     
   }
   
