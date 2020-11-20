3867f57f4820f0334091c96ecc32d110a77f22b4
ravelar <rafaelksd@live.com>
2020-11-19 21:51:49 -0600

    #2 Load the saved shows and fill the cell in the table

--- movie_app/Controllers/FavoritesTableViewController.swift
+++ movie_app/Controllers/FavoritesTableViewController.swift
@@ -13,11 +13,29 @@ class FavoritesTableViewController: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
     
+    self.shows = getShows()
+    self.tableView.reloadData()
   }
   
   // MARK: - Table view data source
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return self.shows?.count ?? 0
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteCell", for: indexPath)
+    let show = self.shows?[indexPath.row]
+    cell.textLabel?.text = show?.name
+    
+    if let imageUrl = show?.medium_image {
+      cell.imageView?.setImage(from: imageUrl, withPlaceholder: UIImage(systemName: "wifi.exclamationmark"))
+    }
+    
+    return cell
   }
 }
