873f88000dd39cdbb367e2abb1814b51f5db2e65
ravelar <rafaelksd@live.com>
2020-11-20 14:14:33 -0600

    #5 Add button config

--- movie_app/Controllers/FavoriteDetailViewController.swift
+++ movie_app/Controllers/FavoriteDetailViewController.swift
@@ -18,20 +18,33 @@ class FavoriteDetailViewController: UIViewController {
    @IBOutlet weak var languageShowDetail: UILabel!
    @IBOutlet weak var statusShowDetail: UILabel!
    @IBOutlet weak var imdbShowDetail: UILabel!
+  @IBOutlet weak var IMDbButton: UIButton!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     // Do any additional setup after loading the view.
-    if let showDetail = show{
+    if let showDetail = show {
       setShowInfo(show: showDetail);
+      
+      if((showDetail.imdb?.isEmpty) != nil){
+        //IMDbButton.isHidden = true
+      }
     }
+    
+  }
+  @IBAction func openIMDb(_ sender: Any) {
+    
+    if let url = URL(string: "https://www.imdb.com/title/\(String(describing: show?.imdb))/") {
+        UIApplication.shared.open(url)
+    }
+    
   }
   
   func setShowInfo(show: ShowEntity){
     
     //Set the image
-    if let imageUrl = show.original_image {
+    if let imageUrl = show.medium_image {
       imageShowDetail.setImage(from: imageUrl, withPlaceholder: UIImage(systemName: "wifi.exclamationmark"))
     }
     
