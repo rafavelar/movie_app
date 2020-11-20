68be6f382ab25a3c0dd15043ebe9af109c5f238a
ravelar <rafaelksd@live.com>
2020-11-20 13:23:53 -0600

    #5 Configure detailFavorite show

--- movie_app/Controllers/FavoriteDetailViewController.swift
+++ movie_app/Controllers/FavoriteDetailViewController.swift
@@ -9,9 +9,40 @@ import UIKit
 
 class FavoriteDetailViewController: UIViewController {
   
+  var show: ShowEntity?
+  
+  @IBOutlet weak var imageShowDetail: UIImageView!
+   @IBOutlet weak var summaryShowDetail: UITextView!
+   
+   @IBOutlet weak var nameShowDetail: UILabel!
+   @IBOutlet weak var languageShowDetail: UILabel!
+   @IBOutlet weak var statusShowDetail: UILabel!
+   @IBOutlet weak var imdbShowDetail: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     // Do any additional setup after loading the view.
+    if let showDetail = show{
+      setShowInfo(show: showDetail);
+    }
+  }
+  
+  func setShowInfo(show: ShowEntity){
+    
+    //Set the image
+    if let imageUrl = show.original_image {
+      imageShowDetail.setImage(from: imageUrl, withPlaceholder: UIImage(systemName: "wifi.exclamationmark"))
+    }
+    
+    //Set summary
+    summaryShowDetail.text = show.summary
+    summaryShowDetail.layer.borderWidth = 1
+    summaryShowDetail.layer.borderColor = UIColor.gray.cgColor
+    
+    nameShowDetail.text = show.name
+    languageShowDetail.text = show.language
+    statusShowDetail.text = show.status
+    imdbShowDetail.text = show.imdb
   }
 }
