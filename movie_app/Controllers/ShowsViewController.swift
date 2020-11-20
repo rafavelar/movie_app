7779053d4af1c81e0cb1ae3209c0d5a47fcc50c6
ravelar <rafaelksd@live.com>
2020-11-19 21:53:21 -0600

    #3 Delete commented lines

--- movie_app/Controllers/ShowsViewController.swift
+++ movie_app/Controllers/ShowsViewController.swift
@@ -14,11 +14,6 @@ class ShowsViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem
     NetworkManager().fetchShows{ (shows) in
       self.shows = shows
       DispatchQueue.main.async {
